ede6f9b3e2385110874b1f5677c443abc065b081
GreenLexNZ <166956742+GreenLexNZ@users.noreply.github.com>
2024-04-29 14:46:30 +0300

    ÐФввштAdding work files

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://7a64a8e2-a618-4b24-858f-5a77e849fa4e.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://a1c1b5e1-4d38-41c5-ae3c-791042699215.serverhub.praktikum-services.ru"
 
 Create_new_order = "/api/v1/orders"
 
